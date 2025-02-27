a653fc17e72550bbf65d18a07b1fcdfde4b061fb
Tyojong <guswjddl4313@naver.com>
2025-02-27 10:40:33 +0900

    update wordpress_crawler&run

--- analysis tool/modules/wordpress_crawler.py
+++ analysis tool/modules/wordpress_crawler.py
@@ -1,18 +1,142 @@
 from bs4 import BeautifulSoup
+import os
+import re
 import requests
+import threading
+
+# 워드프레스 slug 리스트 및 다운로드 url
+slug_list_url = "https://api.wordpress.org/plugins/info/1.2/?action=query_plugins&request[page]="
+slug_download_url = "https://api.wordpress.org/plugins/info/1.0/"
+
+# 워드프레스 slug 리스트 최대 페이지 수
+slug_list_page_max_num = 999
+
+# 저장 파일 및 디렉토리 설정
+plugins_dir = 'plugins/'
+slug_list_dir = 'slug_list/'
+slug_list_file_name = 'slug_list.txt'
+
+# 디렉토리 생성
+if not os.path.exists(plugins_dir):
+    os.makedirs(plugins_dir)
+if not os.path.exists(slug_list_dir):
+    os.makedirs(slug_list_dir)
 
 
-wordpress_plugin_main_url = 'https://ko.wordpress.org/plugins/browse/'
-
-
-class WPCrawlerByBlocks:
+### slug 리스트 가져오기
+class SlugList:
     def __init__(self):
-        self.url = wordpress_plugin_main_url + 'blocks/'
+        self.url = slug_list_url
+        self.page_max = slug_list_page_max_num + 1
+        self.slug_list_file = slug_list_dir + slug_list_file_name
 
-class WPCralwerByBeta:
-    def __init__(self):
-        self.url = wordpress_plugin_main_url + 'beta/'
+    ## 기존 slug 목록 불러오기
+    def load_existing_slugs(self):
+        existing_slugs = {}
 
-class WPCrawlerByPopular:
+        if os.path.exists(self.slug_list_file): # 파일이 존재할 때만 처리
+            with open(self.slug_list_file, 'r') as f:
+                for line in f: #slug_list.txt 파일을 한 줄씩 읽어서 처리
+                    parts = line.strip().split(maxsplit=1)  # 공백을 기준으로 최대 2개의 값만 분리
+
+                    if len(parts) == 2:  # slug와 version이 모두 있는 경우에만 처리
+                        slug, version = parts # slug와 version을 각각 변수에 저장
+                        existing_slugs[slug] = version # 딕셔너리에 추가
+                    else:
+                        print(f"error line: {line.strip()}")  # 로그 출력하여 문제를 파악할 수 있게 함
+        return existing_slugs # 기존 slug 목록 반환
+
+    ## 업데이트된 slug 목록 쓰기
+    def write_updated_slugs(self, existing_slugs):
+        # 기존 슬러그 목록을 모두 다시 쓰기 (업데이트 사항 반영)
+        with open(self.slug_list_file, 'w') as f:  # 'w' 모드로 파일 덮어쓰기 (이전 내용 삭제 -> 새로운 내용 추가)
+            for slug, version in existing_slugs.items(): # 딕셔너리의 key와 value를 각각 slug, version에 저장
+                f.write(f"{slug} {version}\n") 
+
+    ## slug 리스트 가져오기
+    def get_slug_list(self):
+        print("[*]Getting slug list...")
+        existing_slugs = self.load_existing_slugs() # 기존 slug 목록 불러오기
+
+        for page in range(1, self.page_max): # 1부터 page_max까지 반복
+            url = self.url + str(page)
+            response = requests.get(url)
+            data = response.json() # json 형태로 데이터를 가져옴
+
+            updated = False  # 업데이트가 있는지 여부
+
+            for plugin in data['plugins']:
+                slug = plugin['slug'] # 플러그인 슬러그
+                version = plugin['version'] # 플러그인 버전
+                
+                #기존에 슬러그 존재하면 버전 다른지 확인
+                if slug in existing_slugs:
+                    if existing_slugs[slug] != version:
+                        print(f"[>]Update: {slug} {existing_slugs[slug]} -> {version}")
+                        existing_slugs[slug] = version
+                        updated = True
+                #새로운 슬러그
+                else:
+                    print(f"[+]New Plugin: {slug} {version}")
+                    existing_slugs[slug] = version
+                    updated = True
+
+            # 업데이트 즉시 반영
+            if updated:
+                self.write_updated_slugs(existing_slugs)
+
+        
+
+class SlugDownload:
     def __init__(self):
-        self.url = wordpress_plugin_main_url + 'popular/'
+        self.url = slug_download_url
+        self.slug_list_file = slug_list_dir + slug_list_file_name
+
+    def download(self, slug):
+        url = self.url + slug
+        response = requests.get(url)
+
+        result = {}
+        matches = re.findall(r's:(\d+):"(.*?)";|i:(\d+);|b:(\d);|a:(\d+):{|}', response.text)
+    
+        i = 0
+        while i < len(matches):
+            if matches[i][0]:  # s:(length):(string) case
+                key_length = int(matches[i][0])
+                key = matches[i][1]
+                i += 1
+                if matches[i][0]:
+                    value = matches[i][1]
+                elif matches[i][2]:
+                    value = int(matches[i][2])
+                elif matches[i][3]:
+                    value = bool(matches[i][3])
+                else:
+                    value = None  # or parse recursively if it contains nested structures
+                result[key] = value
+            i += 1
+        
+        print("[^]Download Plugin")
+        print(f"slug : {result['slug']}")
+        print(f"version : {result['version']}\n")
+
+        plugin_zip_url = result['trunk']
+
+        plugin_zip = requests.get(plugin_zip_url)
+        with open(plugins_dir + slug + '.zip', 'wb') as f:
+            f.write(plugin_zip.content)
+
+    def read_plugin_list(self, first_idx, count):
+        idx = first_idx - 1
+        with open(self.slug_list_file, 'r') as f:
+            lines = f.readlines()
+            return lines[idx:idx+count]
+        
+    def get_plugin(self, first_idx, count):
+        slugs = self.read_plugin_list(first_idx, count)
+        for slug in slugs:
+            #print(slug.split()[0])
+            self.download(slug.split()[0])
+
+
+
--- analysis tool/run.py
+++ analysis tool/run.py
@@ -1,8 +1,43 @@
-#from modules.wordpress_crawler import class_name
+import optparse
+from modules.wordpress_crawler import SlugList, SlugDownload
 #from modules.exploit.cross_site_scripting import class_name
 #from modules.exploit.sql_injection import class_name
 #from modules.exploit.php_object_injection import class_name
+USAGE = """
+Usage: run.py [options]
+"""
 
 
 if __name__ == "__main__":
-    print('main')
+    parser = optparse.OptionParser('-u <option> -d <option> -x <option> -p <option> -s <option>')
+    parser.add_option('-u', dest='update', type='string', help='update slug list')
+
+    options, args = parser.parse_args()
+
+    print(parser.usage)
+    # try:
+    #     options, args = getopt.getopt(sys.argv[1:], 'u:d:xps')
+    #     print(options)
+
+    #     if options == []:
+    #         print("haha")
+
+    #     for op, p in options:
+    #         if op == '-u' or op == '--update':
+    #             print(f'option -u : {p}')
+    #             #SlugList.get_slug_list()
+    #         elif op == '-d' or op == '--download':
+    #             print(f'option -d : {p}')
+    #             #SlugDownload().download_plugin(p)
+    #         elif op == '-x' or op == '--XSS':
+    #             print(f'option -x : {p}')
+    #         elif op == '-p' or op == '--POI':
+    #             print(f'option -p : {p}')
+    #         elif op == '-s' or op == '--SQL':
+    #             print(f'option -s : {p}')
+
+    # except getopt.GetoptError as e:
+    #     print(USAGE)
+    #SlugList().get_slug_list()
+    #SlugDownload().download_plugin('predictive-search')
+    #SlugDownload().get_plugin(2, 5)
